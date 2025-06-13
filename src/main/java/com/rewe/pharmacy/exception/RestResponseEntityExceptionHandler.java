b0ab9cc69b69bc0d940d73dc33f69c62e49b8e4b
hkostadinova <hkostadinova@gmail.com>
2025-06-13 18:35:47 +0300

    Exceptions Types Added

--- src/main/java/com/rewe/pharmacy/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/rewe/pharmacy/exception/RestResponseEntityExceptionHandler.java
@@ -4,33 +4,78 @@ import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.LinkedHashMap;
+
 @RestControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<Object> handleAuthenticationException(
-            Exception ex, WebRequest request) {
-        return new ResponseEntity<>(
-                "Authentication Failed!", new HttpHeaders(), HttpStatus.BAD_REQUEST);
-    }
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Object> handleAccessDeniedException(
+    @ExceptionHandler({ConstraintViolationException.class, ValidationException.class})
+    public ResponseEntity<Object> handleConstraintViolationException(
             Exception exception, WebRequest request) {
-        return new ResponseEntity<>(
-                "Access denied for this user!", new HttpHeaders(), HttpStatus.BAD_REQUEST);
+        LinkedHashMap<String, String> responseBody = new LinkedHashMap<>();
+        responseBody.put("type", "Constraint Violation Exception");
+        responseBody.put("instance", request.getDescription(false));
+        responseBody.put("status code", String.valueOf(HttpStatus.BAD_REQUEST));
+        return handleExceptionInternal(exception, responseBody,
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        LinkedHashMap<String, String> responseBody = new LinkedHashMap<>();
+        responseBody.put("type", "Method Argument is not valid!");
+        responseBody.put("instance", request.getDescription(false));
+        responseBody.put("message", exception
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getDefaultMessage())
+                .reduce("", (message1, message2) -> message1 + ", " + message2));
+        responseBody.put("status code", String.valueOf(HttpStatus.BAD_REQUEST));
+        return handleExceptionInternal(exception, responseBody,
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
+    public ResponseEntity<Object> handleAuthenticationException(
+            AuthenticationCredentialsNotFoundException exception, WebRequest request) {
+        LinkedHashMap<String, String> responseBody = new LinkedHashMap<>();
+        responseBody.put("type", "Authentication");
+        responseBody.put("instance", request.getDescription(false));
+        responseBody.put("status code", String.valueOf(HttpStatus.UNAUTHORIZED));
+        return handleExceptionInternal(exception, responseBody,
+                new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+    }
+
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<Object> handleObjectNotFoundException(RuntimeException exception, WebRequest request) {
         String responseBody = "Object Not Found!";
-        return handleExceptionInternal(exception, responseBody,
+        return handleExceptionInternal(exception, responseBody + " " + exception.getMessage(),
                 new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> handleAccessDeniedException(
+            Exception exception, WebRequest request) {
+        return new ResponseEntity<>(
+                exception.getMessage(), new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAll(
+            Exception exception, WebRequest request) {
+        String message = "Something went wrong!";
+        return new ResponseEntity<Object>(
+                message, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
